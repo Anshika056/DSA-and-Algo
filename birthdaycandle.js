a679613bee4f2a1d122f9ba0154e34d9e0c9cdf8
anshika056 <anshikamadan2@gmail.com>
2022-03-23 02:27:16 +0530

    max

---
+++ birthdaycandle.js
@@ -0,0 +1,21 @@
+//count the maximum number of candles
+
+function candle(){
+    var arr=[3,2,1,3]
+    let max = arr[0]
+        var count = 0
+    for(i=0;i<arr.length;i++) {    //get the max
+        if(arr[i]>max){
+            max = arr[i]
+        }  
+    }
+    for(i=0;i<arr.length;i++){     //count the number of maximum number
+        if(arr[i]==max){
+            count ++
+        }
+    }
+        return count
+    
+}
+const candles = candle()
+console.log(candles)
