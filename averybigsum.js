f154e774268e7c30c922fe0f19b3e63b83109405
anshika056 <anshikamadan2@gmail.com>
2022-03-23 22:35:00 +0530

    vowel

---
+++ averybigsum.js
@@ -0,0 +1,12 @@
+//option 1
+function aVeryBigSum(ar) {
+    // Write your code here
+      var sum=0
+       for(let i= 0;i<ar.length;i++){
+           sum += ar[i]
+       }
+       return sum
+}
+aVeryBigSum([1000000001,1000000002,1000000003,1000000004,1000000005])
+
+
---
+++ vowel.js
@@ -0,0 +1,16 @@
+//count the number of vowels in sentence
+
+function countvowels(sentence){
+    var count = 0
+    var vowels = ["a","i","e","o","u"]
+    
+    for(let char of sentence){
+        if(vowels.includes(char.toLowerCase())){
+            count++
+        }
+    }
+return count ++
+}
+
+const result = countvowels("hello world");
+console.log(result)
