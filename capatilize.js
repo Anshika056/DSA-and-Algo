f8191df6a8be6a57440b5e29b1ee929cdda35733
anshika056 <anshikamadan2@gmail.com>
2022-03-24 04:00:35 +0530

    captialize

---
+++ capatilize.js
@@ -0,0 +1,12 @@
+function capitalize(str) {
+    const words = [];
+
+    for (let word of str.split(' ')) {
+        words.push(word[0].toUpperCase() + word.slice(1));
+    }
+
+    return words.join(' ');
+}
+
+const result = capitalize("hello world")
+console.log(result)
